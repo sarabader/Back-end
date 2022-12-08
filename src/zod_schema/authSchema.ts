e5bee3c6de5719ba0ceb82ca051e9e95a03aa0bb
Sara <sara@gmail.com>
2022-12-08 16:10:58 +0300

    schema

---
+++ src/zod_schema/authSchema.ts
@@ -0,0 +1,53 @@
+import {z} from 'zod';
+
+export const registerUserSchema = z.object({
+    body: z.object({
+        name: z.
+        string({required_error:'Username is required', invalid_type_error: 'Username must be a string'})
+        .min(3,'Username must be more than 3 char').max(15, 'Username must be less than 15 char'),
+        password: z
+        .string({required_error:'Password is required'})
+        .min(6,'Password must be more than 6 char').max(15, 'Password must be less than 15 char'),
+        email: z         
+        .string({required_error:'Email is required', invalid_type_error: 'Email must be a string'})
+        .email('Please enter a valid email'),
+        phone: z
+        .string({required_error:'Phone is required', invalid_type_error: 'Phone must be a string'})
+        .min(10,'Phone must be 05XXXXXXXX').max(10, 'Phone must be 05XXXXXXXX'),
+       
+    }),
+});  
+
+export const registerConsultantSchema = z.object({
+    body: z.object({
+        name: z.
+        string({required_error:'Username is required', invalid_type_error: 'Username must be a string'})
+        .min(3,'Username must be more than 3 char').max(15, 'Username must be less than 15 char'),
+        password: z
+        .string({required_error:'Password is required'})
+        .min(6,'Password must be more than 6 char').max(15, 'Password must be less than 15 char'),
+        email: z         
+        .string({required_error:'Email is required', invalid_type_error: 'Email must be a string'})
+        .email('Please enter a valid email'),
+        phone: z
+        .string({required_error:'Phone is required', invalid_type_error: 'Phone must be a string'})
+        .min(10,'Phone must be 05XXXXXXXX').max(10, 'Phone must be 05XXXXXXXX'),
+        certificate: z
+        .string({required_error:'Certificate Number is required'}),
+
+        Aboutme:z
+        .string({required_error:'About You is required EX:المؤهل العلمي و سنوات الخبرة '}), 
+    }),
+});  
+
+
+export const loginSchema = z.object({
+    body: z.object({
+        email: z.string({
+            required_error:'email is required', invalid_type_error: 'email must be a string'
+        }),
+        password: z.string({
+            required_error:'Password is required', invalid_type_error: 'Password must be a string'
+        }),
+    })
+});
