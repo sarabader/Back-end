037b9d4c03929fe3bcadd646a77b07fa19c486e0
Sara <sara@gmail.com>
2022-12-11 13:38:24 +0300

    login and register

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,4 +1,4 @@
-    import { Consultant } from '@prisma/client';
+    import { Consultant, User } from '@prisma/client';
 import { Request, Response } from 'express';
 import * as argon2 from 'argon2';
 import * as jwt from 'jsonwebtoken';
@@ -39,23 +39,81 @@ export const ConsultantLoginHandler = async (req: Request, res: Response) => {
 };
 
 
+export const UserLoginHandler = async (req: Request, res: Response) => {
+  const { email, password } = req.body as User;
 
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: 'Wrong Email or Password !',
+    });
+  }
+
+  const isMatched = await argon2.verify(user.password, password);
+
+  if (!isMatched) {
+    return res.status(400).json({
+      message: 'Wrong Email or Password !',
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user.id},
+    process.env.JWT_SECRET as string,
+
+  );
+
+  return res.status(200).json({
+    message: 'Welcome back ' + user.name + ' !',
+    token,
+  });
+};
+
+
+
+export const userRegisterHandler = async (req: Request, res: Response) => {
+  try {
+    const {name, password, email} = req.body as User;
+    const hashedPassword = await argon2.hash(password);
+
+
+    await prisma.user.create({
+      data:{
+          name,
+          password: hashedPassword,
+          email,
+
+      },
+    });
+
+    return res.status(201).json({
+      message:  "تم التسجيل بنجاح !",
+    });
+  } catch (error) {
+    return res.status(400).json({ message: 'Issue with your input' });
+  }
+};
 
 export const ConsultantRegisterHandler = async (req: Request, res: Response) => {
     try {
-      const {name, password, email ,phone, AboutMe, certificate} = req.body as Consultant;
+      const {name, password, email ,phone, AboutMe, certificate, filed} =req.body as Consultant & User;
       const hashedPassword = await argon2.hash(password);
   
 
       await prisma.consultant.create({
-        data:{
+          data:{
             name,
             password: hashedPassword,
             email,
             phone,
             AboutMe, 
-            certificate
+            certificate,
+            filed,
             
+                
         },
       });
   
--- src/route/auth.routes.ts
+++ src/route/auth.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { ConsultantLoginHandler, ConsultantRegisterHandler } from '../controller/auth.controller';
+import { ConsultantLoginHandler, ConsultantRegisterHandler, UserLoginHandler, userRegisterHandler } from '../controller/auth.controller';
 import validate from '../middleware/validate';
-import { loginSchema, registerConsultantSchema } from '../zod_schema/authSchema';
+import { loginSchema, registerConsultantSchema, registerUserSchema } from '../zod_schema/authSchema';
 
 const router = express.Router();
 
 router.post('/register', validate(registerConsultantSchema), ConsultantRegisterHandler);
 router.post('/login', validate(loginSchema), ConsultantLoginHandler);
+
+router.post('/userlogin', validate(loginSchema), UserLoginHandler);
+router.post('/userregister', validate(registerUserSchema), userRegisterHandler);
 
 export default router;
